5b261ec7818298a84c31bd3add2d974be4a7aff6
Jonas Forsberg <jonas@forsberg.co>
2019-04-26 07:31:41 +0200

    ..

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+./add_matomo.sh
 make publish
-rsync -r -a -v -e ssh --delete --ignore-existing output/ web:/srv/www/rre.nu/
+rsync -r -a -v -e ssh --delete output/ web:/srv/www/rre.nu/
